3c8bb07b01615d5873b234ab4934a18a0266f4a3
sonprogrammer <ods04138@gmail.com>
2025-07-15 19:10:47 +0900

    Fix : some error

---
+++ saleCal/src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Weather",
+  description: "Check your Weather",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="w-full p-10 text-center text-xl">
+          <Header />
+        </div>
+        <div className="p-10 border-t-2">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ saleCal/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Easy to bring price
+    </div>
+  )
+}
+
+export default Header
